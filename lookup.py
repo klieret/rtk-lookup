a1ef7719369525ec987c8d49803ece61491324f8
klieret <klieret@users.noreply.github.com>
2015-06-01 16:27:21 +0200

    fixed completely buggy last version

--- lookup.py
+++ lookup.py
@@ -1,110 +1,12 @@
 #!/usr/bin/python3
+ # -*- coding: utf-8 -*-
+
 """
-# Lookup Kanji by Heisig Keyword or frame number
+Lookup Kanji by Heisig Keyword or frame number
 
-## Short Description
-A little command line interface that allows to look up kanji with the
-respective heisig keyword or frame number.
+See https://bitbucket.org/ch4noyu/lookup-kanji-by-heisig-keyword.git
+or http://ch4noyu.bitbucket.org/ for more information.
 
-![lookup.png](https://bitbucket.org/repo/qe4bg9/images/274375207-lookup.png)
-
-Examples:
-
-    inpt: large resist
-    大抵
-
-    inpt: 107 1832
-    大抵
-
-    inpt: large 1832
-    大抵
-
-If words are not found, they are converted to hiragana. This requires the
-```romkan``` module which can be downloaded
-[here](https://pypi.python.org/pypi/romkan).
-
-Examples:
-
-    inpt: large てい
-    大てい
-
-    inpt: large 抵
-    大抵
-
-    inpt: large tei
-    大てい
-
-To quit, type ```.q```.
-
-## Modes
-
-There are three modes (in parenthesis: command to activate mode)
-
-* copy (```.c```): Copy result to clipboard.
-* lookup (```.w```): Lookup expression (default: tangorin.com with firefox)
-* nothing (```.n```): Do nothing.
-
-If more than one match is found, no action will be performed, regardless of the
-current mode.
-The default mode is ```c``` (but this can easily be changed in the source, as
-well as the commands above).
-
-## More on searching
-
-If a keyword contains a space, substitute ```_```:
-
-    inpt: sign_of_the_hog
-    亥
-
-```word+``` will look for all keywords of the form "word1 word2 word3" where
-word matches (exactly) one of the words. If
-there are multiple matches, all of them are printed as a list.
-
-    Inpt: sign+
-    [('酉', 'sign of the bird'), ('亥', 'sign of the hog'), ('寅', 'sign of
-the tiger'), ('辰', 'sign of the dragon'), ('丑', 'sign of the cow'), ('卯',
-'sign of the hare'), ('巳', 'sign of the snake')]
-
-    Inpt: fish+
-    [('乙', 'fish guts'), ('魚', 'fish'), ('鰭', 'fish fin')]
-
-    Inpt: fish
-    魚
-
-    Inpt: fin+
-    鰭
-
-    Inpt: fish+ thunder
-    [('乙', 'fish guts'), ('魚', 'fish'), ('鰭', 'fish fin')]雷
-
-```word?``` will look for all keywords that contain "word":
-
-    Inpt: Inpt: goi?
-    行
-
-    Inpt: fish?
-    [('貝', 'shellfish'), ('乙', 'fish guts'), ('魚', 'fish'), ('漁',
-'fishing'), ('恣', 'selfish'), ('鰭', 'fish fin')]
-
-    Inpt: fish+
-    [('乙', 'fish guts'), ('魚', 'fish'), ('鰭', 'fish fin')]
-
-## Installation:
-
-Download the file ```lookup.py```. And run it with ```python3 lookup.py```.
-
-## Issues, Suggestions, Feature Requests etc.
-Open a ticket at [this addon's gitbucket issue
-page](https://bitbucket.org/ch4noyu/lookup-kanji-by-heisig-keyword/issues?status
-=new&status=open) (prefered method, also works anonymously without login) or
-send me an [e-mail](mailto:ch4noyu@yahoo.com). German is fine, too. I am not a
-professional programmer, so feedback on how to improve my code is welcome, too.
-
-## Source
-The source is hostet at [this addon's bitbucket
-page](https://bitbucket.org/ch4noyu/lookup-kanji-by-heisig-keyword/overview).
-
-## Copyright
 **Copyright:** *ch4noyu* (<mailto:ch4noyu@yahoo.com>)
 
 **Licence:** GNU AGPL, version 3 or later
@@ -116,9 +18,6 @@ with:
 
 **License:** GNU GPL, version 3 or later
 
-## History
-
-* 31 Mai 2015: First version released.
 """
 
 # todo: use the python module for elegant console interfaces
@@ -131,6 +30,9 @@ with:
 import os
 import logging
 import csv
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
 
 # 'romkan' is the module used to convert
 # hiragana to romanji. It is available at https://pypi.python.org/pypi/romkan
@@ -142,8 +44,6 @@ except ImportError:
     logging.debug("Romkan is available at https://pypi.python.org/pypi/romkan.")
     romkanSupport = False
 
-logging.basicConfig(level=logging.DEBUG)
-
 # ---------- CUSTOMIZE ME --------
 
 commandSeparator = "."
@@ -154,6 +54,7 @@ prompt = "Inpt: "
 def copy_to_clipboard(clip):
     """ Copies argument to clipboard. """
     # Check if we are running on linux:
+    success = 1
     if os.name == "posix":
         success = os.system("echo '%s' | xclip -selection c" % clip)
     return success
@@ -162,6 +63,7 @@ def copy_to_clipboard(clip):
 def lookup(clip):
     """ Looks up phrase in the www """
     # Check if we are running on linux:
+    success = 1
     if os.name == "posix":
         success = os.system("firefox http://tangorin.com/general/dict.php?dict=general\&s=%s &" % clip)
     return success
@@ -185,79 +87,91 @@ with open("RTK.tsv", 'r') as csvfile:
 
 
 # ------------ the actual search ------------
-def lookup(search):
+def search(word):
     """
     Does the actual search.
     :param: search
     :return: A list of tuples of the form ("<kanji>", "<keyword>") (or ("<string>", "?") if no kanji is found.)
     """
-    # split up segments
-    segs = search.split(' ')
-    ans = ""
 
-    for seg in segs:
-        found = []
-        seg = seg.replace('_', ' ')
+    word = word.replace('_', ' ')
+    found = []
 
-        if seg.isdigit():
+    if word.isdigit():
+        for kanji in kanjis:
+            if str(kanji[1]) == word:
+                found.append((kanji[0], kanji[3]))
+    else:
+        if word[-1] == "?":
             for kanji in kanjis:
-                if str(kanji[1]) == seg:
+                if word[:-1] in str(kanji[3]):
+                    found.append((kanji[0], kanji[3]))
+        elif word[-1] == "+":
+            for kanji in kanjis:
+                if word[:-1] in str(kanji[3]).split(' '):
                     found.append((kanji[0], kanji[3]))
         else:
-            if seg[-1] == "?":
-                for kanji in kanjis:
-                    if seg[:-1] in str(kanji[3]):
-                        found.append((kanji[0], kanji[3]))
-            elif seg[-1] == "+":
-                for kanji in kanjis:
-                    if seg[:-1] in str(kanji[3]).split(' '):
-                        found.append((kanji[0], kanji[3]))
-            else:
-                for kanji in kanjis:
-                    if seg == str(kanji[3]):
-                        found.append((kanji[0], kanji[3]))
+            for kanji in kanjis:
+                if word == str(kanji[3]):
+                    found.append((kanji[0], kanji[3]))
 
-        # if not found: simply convert to hiragana
-        if not found:
-            if romkanSupport:
-                found.append((romkan.to_hiragana(seg), '?'))
-            else:
-                found.append((seg, '?'))
+    # if not found: simply convert to hiragana
+    if not found:
+        if romkanSupport:
+            found.append((romkan.to_hiragana(seg), '?'))
+        else:
+            found.append((seg, '?'))
 
     return found
 
 # ------------ main loop ------------
 
+
 while True:
-    string = input(prompt)
+    string = input(prompt).strip()
 
     if string == "":
         continue
 
     # Commands
 
+    skip = False
+
     if string == commandSeparator + 'q':
-        break
+        logging.info("Bye.")
+        sys.exit(0)
     if string == commandSeparator + '':
-        continue
+        skip = True
 
     for m in modes:
         if string == commandSeparator + modes[m][0]:
             mode = m
             logging.info("Switched to mode %s." % mode)
+            skip = True
 
-    found = lookup(string)
+    if skip:
+        print()
+        continue
+
+    # split up segments
+    segs = string.split(' ')
+    ans = ""
+
+
 
     # save current mode to temporarily change mode
     tmpMode = mode
 
     ans = ""
-    
-    if len(found) == 1:
-        ans += found[0][0]
-    if len(found) > 1:
-        mode = "x"
-        ans += str(found)
+
+    for seg in segs:
+        hits = search(seg)
+
+        if len(hits) == 1:
+            ans += hits[0][0]
+        if len(hits) > 1:
+            mode = "nothing"
+            ans += str(hits)
 
     print(ans)
     print()
