28aecb6da99b908f37af8a8800f8c53523b6db3c
kavinvin <kavinvin.vin@gmail.com>
2019-08-05 16:27:18 +0700

    Remove classifiers in setup.py

--- setup.py
+++ setup.py
@@ -17,11 +17,6 @@ with open('readme.md') as readme_file:
         packages=find_packages(),
         python_requires='>=3.5',
         include_package_data=True,
-        classifiers=[
-            'Programming Language :: Python :: 3',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-        ],
         entry_points={
             'console_scripts': ['rtk = rtklookup.lookup:main']
         }
